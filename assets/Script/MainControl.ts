33297994d8d76d65189c6a4f3a65f74d2acd5e33
Tung Phan <tungpt.seven@gmail.com>
2020-03-17 17:37:25 +0700

    git init

---
+++ assets/Script/MainControl.ts
@@ -0,0 +1,32 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const { ccclass, property } = cc._decorator
+
+@ccclass
+export default class MainControl extends cc.Component {
+
+    @property(cc.Sprite)
+    spBg: cc.Sprite[] = [null, null];
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start() {
+
+    }
+
+    update(dt) {
+        for (let i = 0;i < this.spBg.length;i++) {
+            this.spBg[i].node.x -= 1.0
+            if (this.spBg[i].node.x <= -288) {
+                this.spBg[i].node.x = 288
+            }
+        }
+    }
+}
